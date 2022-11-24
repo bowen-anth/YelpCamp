2d48000a84545307469565e01896f6c7793804cf
Tony <bowen.anth@gmail.com>
2022-11-24 09:33:45 -0700

    test push

--- public/javascripts/clusterMap.js
+++ public/javascripts/clusterMap.js
@@ -22,7 +22,7 @@ map.addControl(new mapboxgl.NavigationControl());
     clusterMaxZoom: 14, // Max zoom to cluster points on
     clusterRadius: 50 // Radius of each cluster when clustering points (defaults to 50)
     });
-     
+//     
     map.addLayer({
     id: 'clusters',
     type: 'circle',
