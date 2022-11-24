7326b86b3e2fa3955f1a89f8225f621595118375
Tony <bowen.anth@gmail.com>
2022-11-24 08:00:51 -0700

    added custom popups for cluster maps

--- seeds/index.js
+++ seeds/index.js
@@ -24,7 +24,7 @@ const sample = array => array[Math.floor(Math.random() * array.length)];
 
 const seedDB = async() => {
     await Campground.deleteMany({});
-    for (let i = 0; i < 50; i++) {
+    for (let i = 0; i < 300; i++) {
         const price = Math.floor(Math.random() * 20) + 10;
         const random1000 = Math.floor(Math.random() * 1000);
         const camp = new Campground({
@@ -36,7 +36,10 @@ const seedDB = async() => {
             price,
             geometry: {
                 type: "Point",
-                coordinates: [-113.1331, 47.0202]
+                coordinates: [
+                    cities[random1000].longitude,
+                    cities[random1000].latitude,
+                ]
             },
             images: [
                 {
