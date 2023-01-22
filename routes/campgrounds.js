f5b2794f79f26aba5769fec2ffc65377c427540b
Tony <bowen.anth@gmail.com>
2023-01-22 09:11:16 -0700

    Edited campgrounds.js campground id path

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -15,12 +15,25 @@ router.route('/')
 
 router.get('/new', isLoggedIn, campgrounds.renderNewForm);
 
-router.route('/:id')
-    .get(catchAsync(campgrounds.showCampground))
-    .put(isLoggedIn, isAuthor, upload.array('image'), validateCampground, catchAsync(campgrounds.updateCampground))
-    .delete(isLoggedIn, isAuthor, catchAsync(campgrounds.deleteCampground));
+// router.route('/:id')
+//     .get(catchAsync(campgrounds.showCampground))
+//     .put(isLoggedIn, isAuthor, upload.array('image'), validateCampground, catchAsync(campgrounds.updateCampground))
+//     .delete(isLoggedIn, isAuthor, catchAsync(campgrounds.deleteCampground));
 
-
+router.get('/:id', catchAsync(async (req, res,) => {
+    const campground = await Campground.findById(req.params.id).populate({
+        path: 'reviews',
+        populate: {
+            path: 'author'
+        }
+    }).populate('author');
+    console.log(campground);
+    if (!campground) {
+        req.flash('error', 'Cannot find that campground!');
+        return res.redirect('/campgrounds');
+    }
+    res.render('campgrounds/show', { campground });
+}));
 
 router.get('/:id/edit', isLoggedIn, isAuthor, catchAsync(campgrounds.renderEditForm));
 
